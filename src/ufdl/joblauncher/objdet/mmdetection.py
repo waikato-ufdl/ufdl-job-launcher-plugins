7bd857d3b8da35084be00483ef371d8a9b2328bf
Peter Reutemann <fracpete@gmail.com>
2020-09-23 11:47:49 +1200

    basic prediction executor

--- src/ufdl/joblauncher/objdet/mmdetection.py
+++ src/ufdl/joblauncher/objdet/mmdetection.py
@@ -1,7 +1,13 @@
+import csv
 from glob import glob
+import json
+import os
 import shlex
-from ufdl.joblauncher import AbstractDockerJobExecutor
+import traceback
+from ufdl.joblauncher import AbstractDockerJobExecutor, load_class
 from ufdl.pythonclient.functional.object_detection.dataset import download as dataset_download
+from ufdl.pythonclient.functional.object_detection.dataset import get_metadata, set_metadata, set_annotations_for_image
+from ufdl.pythonclient.functional.core.jobs.job import get_output
 
 
 class ObjectDetectionTrain_MMDet_20200301(AbstractDockerJobExecutor):
@@ -150,5 +156,128 @@ class ObjectDetectionTrain_MMDet_20200301(AbstractDockerJobExecutor):
             pk, "mmdetlogtxt", "txt",
             glob(self.job_dir + "/output/*.log"),
             self.job_dir + "/log_txt.zip")
+
+        super()._post_run(template, job, pre_run_success, do_run_success, error)
+
+
+class ObjectDetectionPredict_MMDet_20200301(AbstractDockerJobExecutor):
+    """
+    For executing Tensorflow image classification prediction jobs.
+    """
+
+    def __init__(self, context, config):
+        """
+        Initializes the executor with the backend context.
+
+        :param context: the server context
+        :type context: UFDLServerContext
+        :param config: the configuration to use
+        :type config: configparser.ConfigParser
+        """
+        super(ObjectDetectionPredict_MMDet_20200301, self).__init__(context, config)
+
+    def _pre_run(self, template, job):
+        """
+        Hook method before the actual job is run.
+
+        :param template: the job template to apply
+        :type template: dict
+        :param job: the job with the actual values for inputs and parameters
+        :type job: dict
+        :return: whether successful
+        :rtype: bool
+        """
+        if not super()._pre_run(template, job):
+            return False
+
+        # create directories
+        self._mkdir(self.job_dir + "/prediction")
+        self._mkdir(self.job_dir + "/prediction/in")
+        self._mkdir(self.job_dir + "/prediction/out")
+        self._mkdir(self.job_dir + "/models")
+
+        # download dataset
+        data = self.job_dir + "/data.zip"
+        pk = int(self._input("data", job, template)["value"])
+        options = self._input("data", job, template)["options"]
+        self._log_msg("Downloading dataset:", pk, "-> options='" + str(options) + "'", "->", data)
+        with open(data, "wb") as zip_file:
+            for b in dataset_download(self.context, pk, annotations_args=shlex.split(options)):
+                zip_file.write(b)
+
+        # decompress dataset
+        output_dir = self.job_dir + "/prediction/in"
+        msg = self._decompress(data, output_dir)
+        if msg is not None:
+            raise Exception("Failed to extract dataset pk=%d!\n%s" % (pk, msg))
+
+        # download model
+        model = self.job_dir + "/model.zip"
+        pk = self._pk_from_joboutput(self._input("model", job, template)["value"])
+        with open(model, "wb") as zip_file:
+            for b in get_output(self.context, pk, "model"):
+                zip_file.write(b)
+
+        # decompress model
+        output_dir = self.job_dir + "/output"
+        msg = self._decompress(model, output_dir)
+        if msg is not None:
+            raise Exception("Failed to extract model pk=%d!\n%s" % (pk, msg))
+
+        return True
+
+    def _do_run(self, template, job):
+        """
+        Executes the actual job. Only gets run if pre-run was successful.
+
+        :param template: the job template to apply
+        :type template: dict
+        :param job: the job with the actual values for inputs and parameters
+        :type job: dict
+        """
+
+        image = self._docker_image['url']
+        volumes=[
+            self.job_dir + "/prediction" + ":/prediction",
+            self.job_dir + "/output" + ":/output",
+        ]
+
+        # build model
+        self._run_image(
+            image,
+            volumes=volumes,
+            image_args=shlex.split(self._expand_template(job, template))
+        )
+
+
+    def _post_run(self, template, job, pre_run_success, do_run_success, error):
+        """
+        Hook method after the actual job has been run. Will always be executed.
+
+        :param template: the job template that was applied
+        :type template: dict
+        :param job: the job with the actual values for inputs and parameters
+        :type job: dict
+        :param pre_run_success: whether the pre_run code was successfully run
+        :type pre_run_success: bool
+        :param do_run_success: whether the do_run code was successfully run (only gets run if pre-run was successful)
+        :type do_run_success: bool
+        :param error: any error that may have occurred, None if none occurred
+        :type error: str
+        """
+
+        job_pk = int(job['pk'])
+        dataset_pk = int(self._input("data", job, template)["value"])
+
+        # zip+upload predictions
+        if do_run_success:
+            self._compress_and_upload(
+                job_pk, "predictions", "csv",
+                glob(self.job_dir + "/prediction/out/*.csv"),
+                self.job_dir + "/predictions.zip")
+
+        # post-process predictions
+        if do_run_success and (self._parameter('store-predictions', job, template)['value'] == "true"):
+            pass
 
         super()._post_run(template, job, pre_run_success, do_run_success, error)
