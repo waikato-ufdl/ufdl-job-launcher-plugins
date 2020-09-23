c802c906709d7a629b4a593f0aa0932d85414544
Peter Reutemann <fracpete@gmail.com>
2020-09-24 08:47:23 +1200

    added help and option parsing; added update of repos

--- dev_init.sh
+++ dev_init.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#############
+# FUNCTIONS #
+#############
+
+# the usage of this script
+function usage()
+{
+   echo
+   echo "${0##*/} [-y] [-u] [-h]"
+   echo
+   echo "Initializes the virtual environment for the UFDL job-launcher plugins."
+   echo
+   echo " -h   this help"
+   echo " -y   do not prompt user, assume 'yes'"
+   echo " -u   update any repositories first"
+   echo
+}
+
 function check_executable()
 {
   echo "Checking $EXEC..."
@@ -34,11 +52,47 @@ function check_repository()
     echo "cd .."
     echo "git clone https://github.com/waikato-ufdl/$REPO.git"
     echo	  
-    exit 2
+    exit 1
   else
     echo "...is present"
   fi
 }
+
+function update_repository()
+{
+  echo "Updating repo $REPO..."
+  CURRENT="`pwd`"
+  cd "../$REPO"
+  git pull
+  cd "$CURRENT"
+}
+
+##############
+# PARAMETERS #
+##############
+
+VENV="venv.dev"
+PROMPT="yes"
+UPDATE="no"
+while getopts ":hyu" flag
+do
+   case $flag in
+      y) PROMPT="no"
+         ;;
+      u) UPDATE="yes"
+         ;;
+      h) usage
+         exit 0
+         ;;
+      *) usage
+         exit 1
+         ;;
+   esac
+done
+
+##########
+# CHECKS #
+##########
 
 echo "Performing checks"
 
@@ -79,9 +133,8 @@ then
   PYTHON=python3.8
 else
   echo "Don't know what Python executable to use!"
-  exit 2
+  exit 1
 fi
-
 
 REPO="ufdl-json-messages"
 check_repository
@@ -92,31 +145,53 @@ check_repository
 REPO="ufdl-job-launcher"
 check_repository
 
-echo
-echo "Press any key to start setup of 'venv.dev' for 'UFDL job-launcher-plugins' (using $PYTHON)..."
-read -s -n 1 key
+#############
+# EXECUTION #
+#############
 
-# delete old directory
-if [ -d "./venv.dev" ]
+if [ "$PROMPT" = "yes" ]
 then
-  echo "Removing old virtual environment..."
-  rm -rf ./venv.dev
+  echo
+  echo "Press any key to start setup of '$VENV' for 'UFDL job-launcher-plugins' (using $PYTHON)..."
+  read -s -n 1 key
 fi
 
-echo "Creating new virtual environment..."
-virtualenv -p /usr/bin/$PYTHON ./venv.dev
+# update repos
+if [ "$UPDATE" = "yes" ]
+then
+  REPO="ufdl-json-messages"
+  update_repository
+
+  REPO="ufdl-python-client"
+  update_repository
+
+  REPO="ufdl-job-launcher"
+  update_repository
+
+  git pull
+fi
+
+# delete old directory
+if [ -d "./$VENV" ]
+then
+  echo "Removing old virtual environment..."
+  rm -rf ./$VENV
+fi
+
+echo "Creating new virtual environment $VENV..."
+virtualenv -p /usr/bin/$PYTHON ./$VENV
 
 echo "Installing dependencies..."
-./venv.dev/bin/pip install --upgrade pip
-./venv.dev/bin/pip install --upgrade setuptools
-./venv.dev/bin/pip install Cython
-./venv.dev/bin/pip install numpy
-./venv.dev/bin/pip install tensorflow
-./venv.dev/bin/pip install ../ufdl-json-messages
-./venv.dev/bin/pip install ../ufdl-python-client
-./venv.dev/bin/pip install wai.lazypip
-./venv.dev/bin/pip install wai.annotations
-./venv.dev/bin/pip install psutil
-./venv.dev/bin/pip install pyyaml
-./venv.dev/bin/pip install ../ufdl-job-launcher
-./venv.dev/bin/pip install .
+./$VENV/bin/pip install --upgrade pip
+./$VENV/bin/pip install --upgrade setuptools
+./$VENV/bin/pip install Cython
+./$VENV/bin/pip install numpy
+./$VENV/bin/pip install tensorflow
+./$VENV/bin/pip install ../ufdl-json-messages
+./$VENV/bin/pip install ../ufdl-python-client
+./$VENV/bin/pip install wai.lazypip
+./$VENV/bin/pip install wai.annotations
+./$VENV/bin/pip install psutil
+./$VENV/bin/pip install pyyaml
+./$VENV/bin/pip install ../ufdl-job-launcher
+./$VENV/bin/pip install .
