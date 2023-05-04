869ebeb85ecde2a296182d7bb9c0842d6e2d7850
Peter Reutemann <fracpete@gmail.com>
2023-05-05 10:38:39 +1200

    tweaked check for confidence class

--- src/ufdl/joblauncher/classify/core.py
+++ src/ufdl/joblauncher/classify/core.py
@@ -55,7 +55,8 @@ def calculate_confidence_scores(
         try:
             cls = load_class(c)
             if not issubclass(cls, AbstractConfidenceScore):
-                executor.log_msg(f"Confidence score class '{c}' does not sub-class {AbstractConfidenceScore.__qualname__}")
+                #executor.log_msg(f"Confidence score class '{c}' does not sub-class {AbstractConfidenceScore.__qualname__}")
+                executor.log_msg(f"Confidence score class '{c}' -> '{str(type(cls))}' does not sub-class {str(type(AbstractConfidenceScore))}")
                 continue
             conf_score_obj.append(cls())
         except:
